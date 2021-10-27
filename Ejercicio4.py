a2318e445cf65ef2c97c2f6a0aa4b4c8de92c3fd
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-10-27 11:36:33 +0200

    Pseudocódigo

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,3 +1,16 @@
+# Pseudocódigo
+# 1. Inicio
+# 2. Escribir("Ingrese el número de partidos ganados:")
+# 3. Leer (PG)
+# 4. Escribir("Ingrese el número de partidos empatados:")
+# 5. Leer (PE)
+# 6. Escribir"Ingrese el número de partidos perdidos:")
+# 7. Leer (PP)
+# 8. Procedemos a realizar el cálculo de cada grupo de partidos PPG = PG*3, PPE = PE*1, PPP = PP*0
+# 9. PuntajeFinal = PPG + PPE + PPP
+# 10. Escribir(PF), se imprime el puntaje final
+# 11. Fin
+
 print("Ingrese el número de partidos ganados:")
 PG = int(input())
 
