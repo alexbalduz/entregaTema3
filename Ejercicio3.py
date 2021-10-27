dfc4c53fe092a3ad7207a2021ee390720e5a719a
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-10-27 11:31:57 +0200

    Pseudocódigo

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,3 +1,18 @@
+# Pseudocódigo
+# 1. Inicio
+# 2. Escribir("Ingrese el número de respuestas correctas:")
+# 3. Leer (RC)
+# 4. Escribir("Ingrese el número de respuestas incorrectas:")
+# 5. Leer (RI)
+# 6. Escribir("Ingrese el número de respuestas en blanco:")
+# 7. Leer (RB)
+# 8. Procedemos a realizar el cálculo de cada grupo de respuestas PCR = RC*3, PRI = RI*(-1), PRB = RB*0
+# 9. PF = PCR + PRI + PRB
+# 10. Escribir(PF), se imprime el puntaje final
+# 11. Fin
+
+
+
 print("Ingrese el número de respuestas correctas:")
 RC = int(input())
 
