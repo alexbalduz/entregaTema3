9a892137c9ca5900b1327bf8d1ebb0077f2f85df
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-10-27 11:13:56 +0200

    realizado con pseudocódigo

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,3 +1,14 @@
+# Pseudocódigo
+# 1. Variables: velocidad, tiempo, distancia
+# 2. Inicio
+# 3. Escribir("Ingrese el tiempo en segundos:")
+# 4. Leer tiempo
+# 5. Escribir("Ingrese la velocidad:")
+# 6. Leer distancia
+# 7. distancia = velocidad * tiempo
+# 8. Escribir("La distancia recorrida por el móvil es: " + str(distancia) + " metros")
+# 9. Fin
+
 print("Ingrese el tiempo en segundos:")
 tiempo = float(input())
 
