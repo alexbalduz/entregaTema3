99c97d17ebbd8c0e6cb583615590f5ff5867e43a
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-10-27 11:42:03 +0200

    Pseudocódigo

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,3 +1,11 @@
+# Pseudocódigo
+# 1. Declaramos constantes: LitrosXGalon, PrecioXLitro
+# 2. Inicio
+# 3. Leer(consumo)
+# 4. total = consumo * LitrosXGalon * PrecioXLitro
+# 5. Escribir(total)
+# 6. Fin
+
 #Constantes
 LitrosXGalon = 3.785
 PrecioXLitro = 4.5
