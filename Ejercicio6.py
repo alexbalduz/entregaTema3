6702fc7608fb9ea0956ff785189e58e63868e007
alexbalduz <83299400+alexbalduz@users.noreply.github.com>
2021-10-27 10:11:53 +0200

    realizado

---
+++ Ejercicio6.py
@@ -0,0 +1,20 @@
+#Pedimos números
+print("Ingrese un número:")
+a = int(input())
+print("Ingrese otro número:")
+b = int(input())
+
+suma = a + b
+print(suma)
+
+resta = a - b
+print(resta)
+
+producto = a * b
+print(producto)
+
+divisionEnteros = a // b
+print(divisionEnteros)
+
+divisionFlotante = a / b
+print(divisionFlotante)
